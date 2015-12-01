df566958a87bf69533d3256447fda280bf88aa2e
leechannl <leechannl@icloud.com>
2015-12-01 17:41:27 +0800

    feat (Note): add notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,27 +1,47 @@
 import React from 'react';
-import Notes from './Note.jsx';
+import Notes from './Notes.jsx';
 import uuid from 'node-uuid';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Webpack'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Go laundry'
-  }
-]
-
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // bind `this` in addNote method to `App`
+    this.addNote = this.addNote.bind(this);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Go laundry'
+        }
+      ]
+    };
+  }
   render() {
+    const notes = this.state.notes;
     return (
-      <Notes items={notes}/>
-    )
+      <div>
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <Notes items={notes}/>
+      </div>
+    );
+  }
+  addNote() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: "new task"
+      }])
+    });
   }
 }
 
